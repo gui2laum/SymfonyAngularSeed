a74c868e5a8143fbbd5bbc6a7afa5357d7b132b1
gui2laum <gui2laume@gmail.com>
2015-07-19 16:46:31 +0200

    Gulp tasks build/usemin (2)

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var gulp = require('gulp'),
     bower = require('gulp-bower'),
     livereload = require('gulp-livereload'),
     rename = require('gulp-rename'),
+    es = require('event-stream'),
+    rs = require('run-sequence'),
 
     minifyCss = require('gulp-minify-css'),
     usemin = require('gulp-usemin'),
@@ -56,6 +58,21 @@ gulp.task('clean:tmp', function() {
         .pipe(vinylPaths(del));
 });
 
+gulp.task('clean:tmp:usemin', function() {
+	return gulp.src([
+        folders.tmp + folders.scripts,
+        folders.tmp + files.index
+    ], { read : false }).pipe(vinylPaths(del));
+});
+
+gulp.task('clean:tmp:others', function() {
+	return gulp.src([
+        folders.tmp + folders.images,
+        folders.tmp + folders.css,
+        folders.tmp + folders.views
+    ], { read : false }).pipe(vinylPaths(del));
+});
+
 gulp.task('clean:bower', function() {
 	return gulp.src([
         folders.resource + folders.lib + '*',
@@ -101,18 +118,54 @@ gulp.task('index:dev', [ 'clean:index' ], function() {
 });
 
 // TODO : make the build to prod
-gulp.task('usemin', [ 'clean:resource', 'clean:lib', 'clean:index' ], function() {
-    return gulp.src(folders.index + files.index + '.dist').pipe(usemin({
-            // html : [ minifyHtml({
-            //     empty : true,
-            //     conditionals : true,
-            //     spare : true,
-            //     quotes : true
-            // })],
+gulp.task('usemin:index', [ 'clean:tmp:usemin' ], function() {
+    return gulp.src(folders.index + files.index + '.dist')
+        .pipe(replace(/<script data-usemin=\"js_vendor\" src=\"([0-9a-zA-Z\-_\s\.\/]*)\.js\"><\/script>/g,
+            '<script src="../../' + folders.resource + '$1.min.js"></script>'))
+        .pipe(replace(/<script data-usemin=\"js_app\" src=\"([0-9a-zA-Z\-_\s\.\/]*)\.js\"><\/script>/g,
+            '<script src="../../' + folders.resource + '$1.js"></script>'))
+        .pipe(gulp.dest(folders.tmp));
+});
+
+gulp.task('usemin', [ 'usemin:index' ], function() {
+    return gulp.src(folders.tmp + files.index + '.dist').pipe(usemin({
+            html : [ minifyHtml({
+                empty : true,
+                conditionals : true,
+                spare : true,
+                quotes : true
+            })],
             js_vendor : [ 'concat' ],
-            //js : [ ngmin(), uglify(), 'concat' ]
-        })).pipe(rename(files.index))
+            js_app : [ ngmin(), uglify(), 'concat' ]
+        }))
+        .pipe(gulp.dest(folders.tmp));
+});
+
+gulp.task('resources:others', [ 'clean:tmp:others' ], function() {
+	return gulp.src([
+        folders.resource + folders.css + '**/*.css',
+        folders.resource + folders.images + '**/*',
+        folders.resource + folders.views + '**/*.html'
+    ], { base : folders.resource }).pipe(gulp.dest(folders.tmp));
+});
+
+gulp.task('resources', [ 'usemin', 'resources:others', 'clean:resource', 'clean:lib' ], function() {
+	var stream1 = gulp.src([
+            folders.tmp + folders.css + '**/*.css',
+            folders.tmp + folders.images + '**/*',
+            folders.tmp + folders.scripts + '**/*.js',
+            folders.tmp + folders.views + '**/*.html'
+        ], { base : folders.tmp }).pipe(gulp.dest(webRoot));
+
+    var stream2 = gulp.src(folders.tmp + files.index + '.dist')
+        .pipe(rename(files.index))
         .pipe(gulp.dest(folders.index));
+
+    return es.merge(stream1, stream2);
+});
+
+gulp.task('finalization', function(callback) {
+    rs('resources', 'clean:tmp', callback);
 });
 
 gulp.task('bower', [ 'clean:bower'], function() {
@@ -120,7 +173,7 @@ gulp.task('bower', [ 'clean:bower'], function() {
 });
 
 // High level tasks
-gulp.task('build', [ 'usemin' ]);
+gulp.task('build', [ 'finalization' ]);
 gulp.task('build:dev', [ 'resources:dev', 'lib:dev', 'index:dev' ]);
 gulp.task('install', [ 'bower' ]);
 gulp.task('clean:all', [ 'clean:lib', 'clean:resource', 'clean:install', 'clean:node', 'clean:symfony', 'clean:index' ]);
